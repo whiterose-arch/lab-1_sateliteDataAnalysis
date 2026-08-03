9ff0a75d6cb5c0b3d9355c82c501a179f19cb62b
gitaumaggie <m.gitau1@alustudent.com>
2026-08-03 21:17:33 +0400

    feat(script): add analyze.sh to automate temperature and humidity extraction

---
+++ scripts/analyze.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# This script extracts:
+# 1. Top 10 highest temperatures
+# 2. Humidity data for a selected country
+
+# Check if the user provided a country name
+if [ $# -eq 0 ]; then
+    echo "Usage: ./scripts/analyze.sh <country_name>"
+    exit 1
+fi
+
+# Save the country name passed as an argument
+COUNTRY=$1
+
+# Input dataset
+INPUT="raw_data/satelite_temperature_data.csv"
+
+# Output folder
+OUTPUT="analyzed_data"
+
+# Create the output folder if it doesn't exist
+mkdir -p "$OUTPUT"
+
+#make the output file for the highest temperature data
+touch "$OUTPUT/highest_temp.csv"
+
+ # Preserve header row in output 
+head -n 1 $INPUT > "$OUTPUT/highest_temp.csv"
+
+# Extract the top 10 highest temperatures and save to a new CSV file
+# sort the data by temperature (3rd column) in descending order and take the top 10 rows
+#add the top 10 highest temperatures to the output file
+tail -n +2 $INPUT | sort -t ',' -k 3,3nr | head -n 10 >> "$OUTPUT/highest_temp.csv"
